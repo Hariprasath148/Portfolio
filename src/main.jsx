e2126fbad66aff8822bcb7a038fbd44ef32869a5
Hariprasath148 <janhharigithub@gmail.com>
2024-08-12 21:52:33 +0530

    path home page fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 import Error from './pages/Error';
 
 const router = createBrowserRouter([
-  { path:"/home", element:<Home/>},
+  { path:"/", element:<Home/>},
   { path:"*", element:<Error/>},
 ]);
 
