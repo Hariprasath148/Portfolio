49e7de1df3718161fee9f67b60de5a557ae2f7f7
Hariprasath148 <janhharigithub@gmail.com>
2024-08-12 21:01:44 +0530

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import { NavLink } from 'react-router-dom';
+import '../css/navbar.css';
+
+export default function NavBar() {
+    function opennav(){
+        document.querySelector(".nav_menu").classList.toggle("show");
+        document.querySelector("#ham_burger").classList.toggle("open");
+    }
+    return <>
+        <div className="nav_bar">
+            <div id="ham_burger" className="" onClick={opennav}>
+                <span className="hamburger_line"></span>
+                <span className="hamburger_line"></span>
+                <span className="hamburger_line"></span>
+            </div>
+        </div>
+        <div className="nav_menu row">
+            <div className="nav_menu_one col-lg-8">
+                <div className='nav_link'>
+                    <ul>
+                        <li><span>01</span><NavLink className={({isActive})=>(isActive?"nav_link active":"nav_link")} to="/">HOME</NavLink></li>
+                        <li><span>02</span><NavLink className={({isActive})=>(isActive?"nav_link active":"nav_link")} to="/about">ABOUT</NavLink></li>
+                        <li><span>03</span><NavLink className={({isActive})=>(isActive?"nav_link active":"nav_link")} to="/projects">PROJECTS</NavLink></li>
+                        <li><span>04</span><NavLink className={({isActive})=>(isActive?"nav_link active":"nav_link")} to="/certificates">CERTIFICATES</NavLink></li>
+                    </ul>
+                </div>
+                <div>
+                </div>
+            </div>
+            <div className="nav_menu_two col-lg-4"></div>
+        </div>
+    </>
+}
---
+++ src/pages/Error.jsx
@@ -0,0 +1,24 @@
+import "../css/error.css"
+import ErrorVideo from "../assets/videos/errorvideo.mp4"
+import { NavLink } from 'react-router-dom';
+
+export default function Error() {
+  return <>
+    <div className="row error_container">
+        <div className="col-lg-6 error_inner_containers justify-content-center align-content-center">
+            <div>
+              <p id="error_txt">ERROR 404</p>
+              <p id="error_tag">THIS ISN'T THE PAGE YOU'RE LOOKING FOR</p>
+              <p id="error_para">The page you are looking for does not or no longer exists.</p>
+              <NavLink className="error_home_button" to="/">HOME</NavLink>
+            </div>
+        </div>
+        <div className="col-lg-6 error_inner_containers justify-content-center align-content-center">
+          <video width="100%" height="auto" autoPlay muted loop>
+            <source src={ErrorVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+    </div>
+  </>
+}
