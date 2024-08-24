e5a92ef98c8bb6b1c21f3b008b632861770ce93e
Hariprasath148 <janhharigithub@gmail.com>
2024-08-24 12:25:25 +0530

    path fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import {createHashRouter ,RouterProvider} from "react-router-dom";
 import Home from './pages/Home';
 import Error from './pages/Error';
 import About from './pages/About';
 
-const router = createBrowserRouter([
+const router = createHashRouter ([
   { path:"/", element:<Home/>},
   { path:"*", element:<Error/>},
   { path:"/about", element:<About/>},
-  ],
-  {
-    basename: "/portfolio/", 
-  }
+  ]
 );
 
 createRoot(document.getElementById('root')).render(
